ac06744fee0e5feb3c719de0ac4a42ab7f1f2410
Adam <niewpisze@gmail.com>
2011-10-31 23:18:59 +0100

    validacja

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,3 +1,7 @@
-class Fortune < ActiveRecord::Base
-  attr_accessible :author, :body
-end
+class Fortune < ActiveRecord::Base 
+ attr_accessible :author, :body 
+ validates :body, :presence => true 
+ validates_length_of :body, :in => 2..128 
+ validates_uniqueness_of :body, :case_sensitive => false 
+validates_length_of :author, :in => 4..64, :allow_blank => true
+ end
